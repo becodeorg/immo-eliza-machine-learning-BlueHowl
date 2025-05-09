a441e2c2ce0d3b274deb746bbb923c4c88b2ec09
BlueHowl <avert45officiel@gmail.com>
2025-05-09 14:16:59 +0200

    chore: update repo

--- data_preparator.py
+++ data_preparator.py
@@ -6,7 +6,7 @@ from pipeline.pipeline import prepare
 df = pd.read_csv('./data/Kangaroo.csv')
 
 # Split the dataset into training and testing sets (80% train, 20% test)
-train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
+train_df, test_df = train_test_split(df, test_size=0.2, random_state=100)
 
 # prepare the training and testing data via the pipeline
 prepare(train_df, './data/train_data-cleaned.csv')
